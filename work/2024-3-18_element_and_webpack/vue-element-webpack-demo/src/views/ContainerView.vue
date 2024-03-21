b40b09f59f6301c594d15bd7355b0ec619ea2e59
IzeIzeiii <izeize2503465113@163.com>
2024-03-21 17:20:40 +0800

    js 实现防抖节流

--- work/2024-3-18_element_and_webpack/vue-element-webpack-demo/src/views/ContainerView.vue
+++ work/2024-3-18_element_and_webpack/vue-element-webpack-demo/src/views/ContainerView.vue
@@ -4,21 +4,11 @@
       <el-header class="top">
         <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect"
           background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
-          <el-menu-item index="1">处理中心</el-menu-item>
-          <el-submenu index="2">
-            <template slot="title">我的工作台</template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-            <el-menu-item index="2-3">选项3</el-menu-item>
-            <el-submenu index="2-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="2-4-1">选项1</el-menu-item>
-              <el-menu-item index="2-4-2">选项2</el-menu-item>
-              <el-menu-item index="2-4-3">选项3</el-menu-item>
-            </el-submenu>
-          </el-submenu>
-          <el-menu-item index="3" disabled>消息中心</el-menu-item>
-          <el-menu-item index="4">订单管理</el-menu-item>
+          <el-menu-item @click="myDebouncedCkAdder">自定义防抖加载</el-menu-item>
+          <el-menu-item @click="myThrottleCkAdder">自定义节流加载</el-menu-item>
+          <el-menu-item @click="lodashDebouncedCkAdder">lodash防抖加载</el-menu-item>
+          <el-menu-item @click="lodashThrottleCkAdder">lodash节流加载</el-menu-item>
+          <el-menu-item @click="reset">重置</el-menu-item>
         </el-menu>
       </el-header>
       <el-container>
@@ -77,12 +67,18 @@
 
 <script>
 const axios = require("axios");
+import _ from "lodash";
+
 export default {
   data() {
     return {
       activeIndex: "1",
       activeIndex2: "1",
       tableData: [],
+      myDebouncedCkAdder:this.myDbcd(this.addCkData,1000),
+      myThrottleCkAdder:this.myThrt(this.addCkData,1000),
+      lodashDebouncedCkAdder:_.debounce(this.addCkData,1000),
+      lodashThrottleCkAdder:_.throttle(this.addCkData,1000)
     };
   },
   methods: {
@@ -95,6 +91,37 @@ export default {
     handleClose(key, keyPath) {
       console.log(key, keyPath);
     },
+    myDbcd(fn,t){
+      let tid;
+      return (...args)=>{
+        clearTimeout(tid);
+        tid=setTimeout(()=>{
+          fn(...args);
+        },t);
+      };
+    },
+    myThrt(fn,t){
+      let fl=true;
+      return (...args)=>{
+        if(fl) {
+          fn(...args);
+          fl=false;
+          setTimeout(()=>fl=true,t);
+        };
+      }
+    },
+    addCkData(){
+      axios.get("https://apis.netstart.cn/xpc/home/vmovier").then((res) => {
+        this.tableData=this.tableData.concat(res.data.data.children);
+        console.log(res.data.data.children);
+      });
+    },
+    reset(){
+      axios.get("https://apis.netstart.cn/xpc/home/vmovier").then((res) => {
+        this.tableData=(res.data.data.children);
+        console.log('reset');
+      });
+    }
   },
   mounted: function () {
     axios.get("https://apis.netstart.cn/xpc/home/vmovier").then((res) => {
